a2dfa8b7bb59fa695a410fc00fa73dd037136209
jkharris27 <jkharris27@gmail.com>
2026-09-12 16:51:12 -0500

    Finalize hosted sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='verse-study-v38';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icons/icon.svg'];
+const CACHE='verse-study-v39';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
