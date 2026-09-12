ddf901cdaa18eeeb9ff3cf542e86b1996694df0b
jkharris27 <jkharris27@gmail.com>
2026-09-12 17:35:49 -0500

    Refresh cache for V4.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='verse-study-v42';
+const CACHE='verse-study-v43';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
