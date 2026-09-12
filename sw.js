c6900249bc1ace8563f31e75919f880a35fa7b87
jkharris27 <jkharris27@gmail.com>
2026-09-12 17:27:56 -0500

    Use network-first loading for installed app

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='verse-study-v41';
+const CACHE='verse-study-v42';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
@@ -6,6 +6,12 @@ self.addEventListener('fetch',event=>{
  if(event.request.method!=='GET')return;
  const url=new URL(event.request.url);
  if(url.origin===self.location.origin){
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;})));
+  if(event.request.mode==='navigate'){
+   event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
+    const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response;
+   }).catch(()=>caches.match('./index.html')));
+  }else{
+   event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;})));
+  }
  }
 });
