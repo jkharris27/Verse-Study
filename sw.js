52a39ec28d07cd7ab7b2b822ec96ae9e2332c974
jkharris27 <jkharris27@gmail.com>
2026-09-13 11:27:34 -0500

    Inject V4.7 enhancements and refresh app cache

--- sw.js
+++ sw.js
@@ -1,17 +1,27 @@
-const CACHE='verse-study-v47';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./share-qr.svg'];
+const CACHE='verse-study-v48';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./share-qr.svg','./v47.js'];
+function injectEnhancements(html){
+ if(html.includes('v47.js'))return html;
+ return html.replace('</body>','<script src="./v47.js"></script></body>');
+}
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
  if(event.request.method!=='GET')return;
  const url=new URL(event.request.url);
- if(url.origin===self.location.origin){
-  if(event.request.mode==='navigate'){
-   event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
-    const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response;
-   }).catch(()=>caches.match('./index.html')));
-  }else{
-   event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;})));
-  }
+ if(url.origin!==self.location.origin)return;
+ if(event.request.mode==='navigate'){
+  event.respondWith(fetch(event.request,{cache:'no-store'}).then(async response=>{
+   const html=injectEnhancements(await response.clone().text());
+   const out=new Response(html,{status:response.status,statusText:response.statusText,headers:{'Content-Type':'text/html; charset=utf-8'}});
+   const copy=out.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return out;
+  }).catch(async()=>{
+   const cached=await caches.match('./index.html');
+   if(!cached)return cached;
+   const html=injectEnhancements(await cached.text());
+   return new Response(html,{headers:{'Content-Type':'text/html; charset=utf-8'}});
+  }));
+ }else{
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;})));
  }
 });
