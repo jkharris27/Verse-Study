0fd11c2f2e0e0b0830e60f5b956bd9f9b4571a1c
jkharris27 <jkharris27@gmail.com>
2026-09-13 11:41:51 -0500

    Make pull-to-refresh fetch latest app code

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE='verse-study-v49';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./share-qr.svg','./v47.js'];
+const CACHE='verse-study-v50';
+const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./share-qr.svg'];
 function injectEnhancements(html){
  if(html.includes('v47.js'))return html;
- return html.replace('</body>','<script src="./v47.js"></script></body>');
+ return html.replace('</body>','<script src="./v47.js?v=50"></script></body>');
 }
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
@@ -13,7 +13,7 @@ self.addEventListener('fetch',event=>{
  if(event.request.mode==='navigate'){
   event.respondWith(fetch(event.request,{cache:'no-store'}).then(async response=>{
    const html=injectEnhancements(await response.clone().text());
-   const out=new Response(html,{status:response.status,statusText:response.statusText,headers:{'Content-Type':'text/html; charset=utf-8'}});
+   const out=new Response(html,{status:response.status,statusText:response.statusText,headers:{'Content-Type':'text/html; charset=utf-8','Cache-Control':'no-store'}});
    const copy=out.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return out;
   }).catch(async()=>{
    const cached=await caches.match('./index.html');
@@ -21,7 +21,13 @@ self.addEventListener('fetch',event=>{
    const html=injectEnhancements(await cached.text());
    return new Response(html,{headers:{'Content-Type':'text/html; charset=utf-8'}});
   }));
- }else{
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;})));
+  return;
  }
+ if(url.pathname.endsWith('/v47.js')||url.pathname.endsWith('/manifest.webmanifest')){
+  event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
+   const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;
+  }).catch(()=>caches.match(event.request)));
+  return;
+ }
+ event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;})));
 });
