aa2e79b7f997a23b7f6c6f23502ca8c1bbc7da00
jkharris27 <jkharris27@gmail.com>
2026-09-13 10:55:27 -0500

    Refresh app shell for Verse Study V4.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='verse-study-v45';
+const CACHE='verse-study-v46';
 const SHELL=['./','./index.html','./manifest.webmanifest','./icon.svg','./share-qr.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
