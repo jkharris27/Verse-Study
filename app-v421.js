98665e3e048239da9cf8f018993cd8911e403266
jkharris27 <jkharris27@gmail.com>
2026-09-19 18:04:16 -0500

    Release V4.48 narrower internal scrollers

--- app-v421.js
+++ app-v421.js
@@ -1,5 +1,5 @@
 (()=>{'use strict';
-const VERSION='V4.47', SHARE_URL='https://jkharris27.github.io/Verse-Study/';
+const VERSION='V4.48', SHARE_URL='https://jkharris27.github.io/Verse-Study/';
 const $=id=>document.getElementById(id);
 const SAFE_STORAGE={getItem:k=>{try{return localStorage.getItem(k)}catch{return null}},setItem:(k,v)=>{try{localStorage.setItem(k,v)}catch{}},removeItem:k=>{try{localStorage.removeItem(k)}catch{}}};
 const names=['Genesis','Exodus','Leviticus','Numbers','Deuteronomy','Joshua','Judges','Ruth','1 Samuel','2 Samuel','1 Kings','2 Kings','1 Chronicles','2 Chronicles','Ezra','Nehemiah','Esther','Job','Psalms','Proverbs','Ecclesiastes','Song of Solomon','Isaiah','Jeremiah','Lamentations','Ezekiel','Daniel','Hosea','Joel','Amos','Obadiah','Jonah','Micah','Nahum','Habakkuk','Zephaniah','Haggai','Zechariah','Malachi','Matthew','Mark','Luke','John','Acts','Romans','1 Corinthians','2 Corinthians','Galatians','Ephesians','Philippians','Colossians','1 Thessalonians','2 Thessalonians','1 Timothy','2 Timothy','Titus','Philemon','Hebrews','James','1 Peter','2 Peter','1 John','2 John','3 John','Jude','Revelation'];
